b44e926b52dc5fb509facefcdeb85fb7c2b87abd
anjxxyi <anjxxyi@gmail.com>
2024-03-22 11:21:38 +0900

    Fix a label component

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -4,5 +4,5 @@ interface ILabelProps {
 }
 
 export default function Label({ htmlFor, children }: ILabelProps) {
-    return <label htmlFor={htmlFor}>{children}</label>;
+    return <label className="text-sm text-primary" htmlFor={htmlFor}>{children}</label>;
 }

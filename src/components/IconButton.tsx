3f0162fca2ddf54ea4b3a0c736010056ae9a01b2
anjxxyi <anjxxyi@gmail.com>
2024-03-22 14:58:53 +0900

    Create an icon button component

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,13 @@
+interface IIconButtonProps {
+    alt?: string;
+    iconPath?: string;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function IconButton({ alt, iconPath, onClick }: IIconButtonProps) {
+    return (
+        <button onClick={onClick}>
+            <img className="w-5" src={iconPath} alt={alt} />
+        </button>
+    )
+}
---
+++ src/stories/IconButton.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import IconButton from "../components/IconButton";
+
+const meta = {
+  title: "Buttons/IconButton",
+  component: IconButton,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    alt: { control: "text", description: "이미지의 alt 속성", defaultValue: "icon" },
+    iconPath: { control: "text", description: "이미지의 경로", defaultValue: "" },
+    onClick: { action: "clicked", description: "이미지의 클릭 이벤트" },
+
+  },
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    alt: "icon",
+    iconPath: "/icons/ic-delete-dark.svg",
+  },
+};

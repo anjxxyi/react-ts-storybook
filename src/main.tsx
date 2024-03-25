886943300b6af4b3e48cd37d40acc6aef588b9b8
anjxxyi <anjxxyi@gmail.com>
2024-03-25 16:44:19 +0900

    Add reset styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./reset.css";
 import "./index.css";
 import "@fontsource/noto-sans-kr"; // Defaults to weight 400
 

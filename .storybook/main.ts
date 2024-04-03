318da2ea5da377c412443942b7b3eaafff59d5c5
anjxxyi <anjxxyi@gmail.com>
2024-04-03 10:38:25 +0900

    Fix chromatic in package.json

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,9 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
+    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
   ],

7a5bd4cd7a9be16bf666eb464189dab6537351bf
anjxxyi <anjxxyi@gmail.com>
2024-04-08 15:32:42 +0900

    Remove coverage test

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,7 +8,6 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
-    '@storybook/addon-coverage',
   ],
   framework: {
     name: "@storybook/react-vite",
